ac3e715aa2c7908e8f5106f777f54cf234fb9ea4
Colin_lsplt <colinlespi@gmail.com>
2024-08-18 15:11:10 +0200

    test

---
+++ src/Components/navbar.jsx
@@ -0,0 +1,17 @@
+import react from 'React';
+import './navbar.css';
+
+const navbar = () => {
+  return (
+      <nav>
+          <ul>
+              <li>
+                  <a href={} >Test</a>
+              </li>
+          </ul>
+
+      </nav>
+  )
+}
+
+export default navbar();
